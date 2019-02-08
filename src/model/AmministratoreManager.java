c44101a0da1ef1541e6c1455419050761a4fa3ee
giovanni992 <giovanni.buonocore97@gmail.com>
2019-02-08 15:27:27 +0100

    revisione amministratore manager

--- src/model/AmministratoreManager.java
+++ src/model/AmministratoreManager.java
@@ -32,7 +32,7 @@ public class AmministratoreManager {
 			if(libro.getIsbn()==null || libro.getIsbn().length()!=13) throw new IllegalArgumentException("Isbn non corretto");
 			if(libro.getTitolo()==null || libro.getTitolo().length()>100) throw new IllegalArgumentException("Titolo non corretto");
 			if(libro.getTrama()==null || libro.getTrama().length()>1700) throw new IllegalArgumentException("Trama non corretta");
-			if(libro.getCasaEditrice()==null || libro.getCasaEditrice().length()!=30) throw new IllegalArgumentException("Casa Editrice non corretta");
+			if(libro.getCasaEditrice()==null || libro.getCasaEditrice().length()>30) throw new IllegalArgumentException("Casa Editrice non corretta");
 			if(libro.getPrezzo()<=0) throw new IllegalArgumentException("Prezzo non corretto");
 			if(libro.getQuantità()==0) throw new IllegalArgumentException("Quantità non corretta");
 			if(libro.getCategoria()==null || libro.getCategoria().length()>30) throw new IllegalArgumentException("Categoria non corretta");
