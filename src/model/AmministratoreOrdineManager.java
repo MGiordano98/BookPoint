95c07d78191a2ff4d67c11a4f0945f22174e7a3b
GennaroTeodoro <40496530+GtGith@users.noreply.github.com>
2019-02-08 14:13:37 +0100

    removing a never used import

--- src/model/AmministratoreOrdineManager.java
+++ src/model/AmministratoreOrdineManager.java
@@ -21,33 +21,37 @@ public class AmministratoreOrdineManager {
 	public Ordine ricercaOrdine(int numOrdine) throws SQLException {
 		Connection connection= DriverMaagerConnectionPool.getConnection();
 		PreparedStatement pStatement= null;
-		
+
 		String selectQ= "SELECT * FROM ordine WHERE numero = ?";
 		Ordine ordine=null;
-		
-		try {
-			pStatement= connection.prepareStatement(selectQ);
-			pStatement.setInt(1, numOrdine);
-			ResultSet rs= pStatement.executeQuery();
-			
-			if(rs.next()) {
-				ordine=new Ordine();
-				ordine.setCittà(rs.getString("città"));
-				ordine.setDataEffettuata(rs.getDate("dataOrdine"));
-				ordine.setDataConsegna(rs.getDate("dataConsegna"));
-				ordine.setOra(rs.getTime("oraConsegna"));
-				ordine.setIdOrdine(rs.getInt("numero"));
-				ordine.setStato(rs.getString("stato"));
-			}
-		}finally {
+		try{
+			if(numOrdine<=0) throw new IllegalArgumentException("Numero ordine errato");
 			try {
-				if(pStatement!=null) pStatement.close();
+				pStatement= connection.prepareStatement(selectQ);
+				pStatement.setInt(1, numOrdine);
+				ResultSet rs= pStatement.executeQuery();
+
+				if(rs.next()) {
+					ordine=new Ordine();
+					ordine.setCittà(rs.getString("città"));
+					ordine.setDataEffettuata(rs.getDate("dataOrdine"));
+					ordine.setDataConsegna(rs.getDate("dataConsegna"));
+					ordine.setOra(rs.getTime("oraConsegna"));
+					ordine.setIdOrdine(rs.getInt("numero"));
+					ordine.setStato(rs.getString("stato"));
+				}
 			}finally {
-				DriverMaagerConnectionPool.releaseConnection(connection);
+				try {
+					if(pStatement!=null) pStatement.close();
+				}finally {
+					DriverMaagerConnectionPool.releaseConnection(connection);
+				}
 			}
+		}catch(IllegalArgumentException e){
+			e.printStackTrace();
 		}
 		return ordine;
-		
+
 	}
 
 	/**
@@ -59,34 +63,34 @@ public class AmministratoreOrdineManager {
 	public boolean cambiaStato(int numOrdine, String stato) throws SQLException {
 		Connection connection= DriverMaagerConnectionPool.getConnection();
 		PreparedStatement pStatement= null;
-		
+
 		boolean result= false;
 		String updateQ= "UPDATE ordine SET stato = ? WHERE numero = ?";
-		
+
 		try{
 			if(numOrdine<=0) throw new IllegalArgumentException("Numero ordine errato");
 			if(!stato.equals("In preparazione") &&  !stato.equals("In transiro") && !stato.equals("In consegna") && !stato.equals("Consegnato")) throw new IllegalArgumentException("Stato errato");
-		try {
-			pStatement= connection.prepareStatement(updateQ);
-			pStatement.setString(1, stato);
-			pStatement.setInt(2, numOrdine);
-			if(pStatement.executeUpdate()==1) {
-				result= true;
-			}
-			connection.commit();
-		}finally {
 			try {
-				if(pStatement!=null) pStatement.close();
+				pStatement= connection.prepareStatement(updateQ);
+				pStatement.setString(1, stato);
+				pStatement.setInt(2, numOrdine);
+				if(pStatement.executeUpdate()==1) {
+					result= true;
+				}
+				connection.commit();
 			}finally {
-				DriverMaagerConnectionPool.releaseConnection(connection);
+				try {
+					if(pStatement!=null) pStatement.close();
+				}finally {
+					DriverMaagerConnectionPool.releaseConnection(connection);
+				}
 			}
-		}
 		}catch(IllegalArgumentException e){
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @param numOrdine
@@ -97,30 +101,30 @@ public class AmministratoreOrdineManager {
 	public boolean cambiaDataEOra(int numOrdine, Date data, Time ora) throws SQLException {
 		Connection connection= DriverMaagerConnectionPool.getConnection();
 		PreparedStatement pStatement= null;
-		
+
 		boolean result= false;
 		String updateQ= "UPDATE ordine SET dataConsegna = ?, oraConsegna = ? WHERE numero = ?";
 		try{
-			
+
 			if(numOrdine<=0) throw new IllegalArgumentException("Numero ordine errato");
 			if(data==null) throw new IllegalArgumentException("Data errata");
 			if(ora==null) throw new IllegalArgumentException("Ora errata");
-		try {
-			pStatement= connection.prepareStatement(updateQ);
-			pStatement.setDate(1, data);
-			pStatement.setTime(2, ora);
-			pStatement.setInt(3, numOrdine);
-			if(pStatement.executeUpdate()==1) {
-				result= true;
-			}
-			connection.commit();
-		}finally {
 			try {
-				if(pStatement!=null) pStatement.close();
+				pStatement= connection.prepareStatement(updateQ);
+				pStatement.setDate(1, data);
+				pStatement.setTime(2, ora);
+				pStatement.setInt(3, numOrdine);
+				if(pStatement.executeUpdate()==1) {
+					result= true;
+				}
+				connection.commit();
 			}finally {
-				DriverMaagerConnectionPool.releaseConnection(connection);
+				try {
+					if(pStatement!=null) pStatement.close();
+				}finally {
+					DriverMaagerConnectionPool.releaseConnection(connection);
+				}
 			}
-		}
 		}catch(IllegalArgumentException e){
 			e.printStackTrace();
 		}
